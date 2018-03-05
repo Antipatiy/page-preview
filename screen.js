ea8ff287a6e3b1b0ef083b1420f7a0fe809bceed
Yury Krutko <yury.krutko@technavology.com>
2018-03-06 00:01:14 +0300

    Added 404 error

--- screen.js
+++ screen.js
@@ -6,22 +6,24 @@ const exec = require('child_process').exec;
 const path = './src/screens';
 
 // remove screens directory
-exec('rm -r ' + path, function (err) {
-
-});
+exec('rm -r ' + path, function (err) {});
 
 // create screens directory
 if (!fs.existsSync(dir)){
   fs.mkdirSync(dir);
-
 }
 
 const takeScreenShot = async (urlData, name) => {
   const instance = await phantom.create();
   const page = await instance.createPage();
-  const status = await page.open(urlData);
+  let status = await page.open(urlData);
   console.log(name + ' ' + status);
   if (status === 'success') {
+    let fileName = name + '.png';
+    await page.render('src/screens/' + fileName);
+  }
+  if (status === 'fail') {
+    await page.open('https://i.stack.imgur.com/WOlr3.png');
     let fileName = name + '.png';
     await page.render('src/screens/' + fileName);
   }
@@ -31,5 +33,3 @@ const takeScreenShot = async (urlData, name) => {
 for (let i = 0; i < urlData.length; i++) {
   takeScreenShot(urlData[i].url, urlData[i].name);
 }
-
-// to run -> node index.js
