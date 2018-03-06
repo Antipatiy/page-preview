7ef53e97dbcce51f20b0de77d8e586da77502f29
Yury Krutko <yury.krutko@technavology.com>
2018-03-06 11:08:53 +0300

    refactored phantom driven render code

--- screen.js
+++ screen.js
@@ -23,21 +23,20 @@ class Screen {
   async takeScreenshot (urlData, name) {
     const instance = await this.phantom.create();
     const page = await instance.createPage();
-    let status = await page.open(urlData);
-
-    console.log(name + ' ' + status);
+    const status = await page.open(urlData);
+    const fileName = name + '.png';
 
     if (status === 'success') {
-      let fileName = name + '.png';
       await page.render(this.screenPath + fileName);
     }
 
-    if (status === 'fail') {
+    else {
       await page.open(this.errorScreen);
-      let fileName = name + '.png';
       await page.render(this.screenPath + fileName);
     }
-    await instance.exit()
+    console.log(name + ' ' + status);
+
+    await instance.exit();
   }
 
   renderAllScreens () {
@@ -54,44 +53,3 @@ class Screen {
 }
 
 new Screen().init();
-
-//------------------------------------------------------------------
-//
-// const phantom = require('phantom');
-// const urlData = require('./src/data/url.json');
-// const fs = require('fs');
-// const dir = './src/screens';
-// const exec = require('child_process').exec;
-// const path = './src/screens';
-//
-// // remove screens directory
-// exec('rm -r ' + path, function (err) {});
-//
-// // create screens directory
-// if (!fs.existsSync(dir)){
-//   fs.mkdirSync(dir);
-// }
-//
-// const takeScreenShot = async (urlData, name) => {
-//   const instance = await phantom.create();
-//   const page = await instance.createPage();
-//   let status = await page.open(urlData);
-//
-//   console.log(name + ' ' + status);
-//
-//   if (status === 'success') {
-//     let fileName = name + '.png';
-//     await page.render('src/screens/' + fileName);
-//   }
-//
-//   if (status === 'fail') {
-//     await page.open('https://i.stack.imgur.com/WOlr3.png');
-//     let fileName = name + '.png';
-//     await page.render('src/screens/' + fileName);
-//   }
-//   await instance.exit()
-// };
-//
-// for (let i = 0; i < urlData.length; i++) {
-//   takeScreenShot(urlData[i].url, urlData[i].name);
-// }
